6419b34b9f753b5d83f87ba5553e2e11b0bf0b26
David Akpan <Davidakpan247@gmail.com>
2023-06-15 09:35:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - This program prints and output using puts
- * Return: 0 (successful)
- */
+* main - This program prints and output using puts
+* Return: 0 (successful)
+*/
 {
 int main(void)
 puts("\"Programming is like building a multilingual puzzle\n");

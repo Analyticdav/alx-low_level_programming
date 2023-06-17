fd30a11201de6f1dcf1e812053f8e2add50c2e62
David Akpan <Davidakpan247@gmail.com>
2023-06-17 04:35:03 -0700

    really

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program that prints numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for
+		(num = 0; num < 0; num++);
+	putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
+

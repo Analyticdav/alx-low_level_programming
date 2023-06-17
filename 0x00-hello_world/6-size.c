cfbbe3588654d78023459da039a5efcba4c8abaf
David Akpan <Davidakpan247@gmail.com>
2023-06-17 12:15:37 -0700

    size of variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This program prints different data types
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("size of a char: %lu bytes\n", (unsigned long)sizeof(a));
+printf("size of an int : %lu bytes\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu bytes\n", (unsigned long)sizeof(c));
+printf("size of  long long int: %lu bytes\n", (unsigned long)sizeof(d));
+0printf("size of a float: %lu bytes\n", (unsigned long)sizeof(f));
+		return (0);
+}

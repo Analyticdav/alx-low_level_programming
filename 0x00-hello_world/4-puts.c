cb9fecb98ff3ff89c163672e9c332674094fee4e
David Akpan <Davidakpan247@gmail.com>
2023-06-17 10:51:34 -0700

    programming puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - This program prints and output using puts
-* Return: 0 (successful)
-*/
-{
+ *  main - This program prints and output using puts
+ *  Return: 0 (successful)
+ */
 int main(void)
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+		{
+		puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }

939daac8f9b75be8a3a487940fe122ac871db04f
David Akpan <Davidakpan247@gmail.com>
2023-06-22 08:06:20 -0700

    positive or negative numbers

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Defines the function
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int i)
+int main(void)
+int main(void)
+int main(void)
+
+#endif

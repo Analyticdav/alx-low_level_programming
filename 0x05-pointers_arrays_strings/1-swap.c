366c3f2383c25d33d55351aee466262c71e94df0
David Akpan <Davidakpan247@gmail.com>
2023-06-26 02:38:01 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - the function to swap files
+ * @a: The first character
+ * @b: The second character
+ */
+void swap_int(int *a, int *b)
+{
+int m;
+
+m = *a;
+*a = *b;
+*b = m;
+}

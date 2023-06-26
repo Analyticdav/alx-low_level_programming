9a0c4b163c95c058bf0793ba5784e8f83794e30d
David Akpan <Davidakpan247@gmail.com>
2023-06-26 03:32:34 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - This function prints in reverse
+ * @s:The string to output
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int l;
+int m;
+while (*s = '\0')
+{
+l++;
+s++;
+}
+s--;
+for (m = l; m > 0; m--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}

97013e4a551ef2a845fa485a8d0b67b4c8b25579
David Akpan <Davidakpan247@gmail.com>
2023-07-01 06:31:46 -0700

    fiz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - The function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int p;
+
+for (p = 1; p <= 100; p++)
+{
+if (p % 3 == 0 && p % 5 != 0)
+{
+printf("Fizz");
+}
+else if (p % 5 == 0 && p % 3 != 0)
+{
+printf("Buzz");
+}
+else if (p % 3 == 0 && p % 5 == 0)
+{
+printf("FizzBuzz");
+}
+}
+printf("\n");
+return (0);
+}
+

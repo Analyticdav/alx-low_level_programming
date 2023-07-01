c70762b451a4659f6fc8acb00f980474b0aa7fca
David Akpan <Davidakpan247@gmail.com>
2023-07-01 03:07:36 -0700

    A program to print a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,3 +1,22 @@
 #include "main.h"
-**
-
+/**
+ * print_line - A function to print lines
+ * Return: void
+ * @n: integer to print
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int k;
+for (k = 1; k <= n; k++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}

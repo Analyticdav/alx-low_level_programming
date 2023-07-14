55cc7a0a5127f4426f75088556fc7adbb493bedf
David Akpan <Davidakpan247@gmail.com>
2023-07-14 12:19:40 -0700

    function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
- * _strncpy - The function for string catenation
- * @src: The input
- * @dest: the output
- * @n: The counter
- * Return: pointer to dest
+ * _strncpy - Copies a string
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of characters to copy
+ *
+ * Return: Pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int bytes_count;
+	char *dest_start = dest;
+	int i;
 
-for (bytes_count = 0; bytes_count < n && src[bytes_count] != '\0'; bytes_count++)
-dest[bytes_count] = src[bytes_count];
-for (; bytes_count < n; bytes_count++)
-dest[bytes_count] = '\0';
-return (dest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest_start);
 }

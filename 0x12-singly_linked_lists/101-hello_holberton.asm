f76d213b1b59b22e3406ac0534e2fbfee78f64c8
David Akpan <Davidakpan247@gmail.com>
2023-07-28 03:21:53 -0700

    p

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,15 @@
-global	main
-	extern	printf
+section .data
+    format db 'Hello, Holberton', 0   ; Thisstore a null-terminated string
+
+section .text
+global main
+extern printf
+
 main:
-	mov	edi,	format
-	xor	eax,	eax
-	call	printf
-	mov	eax, 0
-format: db 'Hello, Holberton\n', 0
+    push format             ; the address of the formated string is pished onto the stack
+    call printf             ; Calling the printf function
+    add esp, 4              ; stack clean up after the call
+
+    xor eax, eax            ; the return value is det to zero
+    ret                     ; Return
+

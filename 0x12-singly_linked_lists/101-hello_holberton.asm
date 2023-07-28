6580d6bc1369f503310aad93052a7a67f0e3d2e3
David Akpan <Davidakpan247@gmail.com>
2023-07-28 03:16:04 -0700

    p

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,8 @@
-section .data
-    hello_msg db "Hello, Holberton", 0    ; Null-terminated string to be printed
-
-section .text
-    global main
-    extern printf
-
+global	main
+	extern	printf
 main:
-    push rbp                        ; Save the base pointer
-    mov rdi, hello_msg              ; Set the first argument (format string)
-    call printf                     ; Call the printf function
-    pop rbp                         ; Restore the base pointer
-
-    ; Clean up and exit the program
-    xor rax, rax                    ; Set the return value to 0 (success)
-    ret                             ; Return from main
-
+	mov	edi,	format
+	xor	eax,	eax
+	call	printf
+	mov	eax, 0
+format: db 'Hello, Holberton\n', 0
